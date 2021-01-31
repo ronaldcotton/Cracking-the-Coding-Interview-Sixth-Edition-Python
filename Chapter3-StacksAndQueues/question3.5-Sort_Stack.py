b3218b8e1cb11380a7a4843115a08f42bd2fa71f
Ronald Cotton <roncotton@gmail.com>
2021-01-30 23:56:45 -0800

    started Q3.5 - needs debugging

---
+++ Chapter3-StacksAndQueues/question3.5-Sort_Stack.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Question 3.5 - Sort Stack
+# Hints - 15, 32, 43
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.temp = []
+    
+    def __str__(self):
+        return str(self.stack)
+
+    # --- stack commands - LIFO ---
+    # remove last element from array
+    def pop(self):
+        if len(self.stack) != 0:
+            return self.stack.pop()
+    
+    # stack = 1 2 4 5, temp_stack: 5 4 2 1 value = 3
+    def push(self, value):
+        return self.stack.append(value)
+    
+    def peek(self):
+        len_stack = len(self.stack)
+        if len_stack != 0:
+            return self.stack[len_stack-1]
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+def sort(stack):
+	temp_stack = Stack()
+	temp2_stack = Stack()
+	
+	if stack.isEmpty():
+		return
+	
+	temp_stack.push(stack.pop())
+	
+	while not stack.isEmpty():	
+		if temp_stack.peek() < stack.peek():
+			temp_stack.push(stack.pop())
+		else:
+			while not temp_stack.isEmpty():
+				temp2_stack.push(temp_stack.pop())
+			temp_stack.push(stack.pop())
+			while not temp2_stack:
+				temp_stack.push(temp2_stack.pop())
+
+	while not temp_stack.isEmpty():
+		stack.push(temp_stack.pop())
+
+	return stack 
+
+if __name__ == "__main__":
+    s = Stack()
+
+    # testing stack
+    print(s.isEmpty())
+    s.push(2)
+    s.push(20)
+    s.push(15)
+    s.push(111)
+    s.push(1)
+    #print(sort(s))
+    print(s)
+    print(sort(s))
+    print(s)
+    print(s.pop())
+    print(s.peek())
+    print(s)
+
+
